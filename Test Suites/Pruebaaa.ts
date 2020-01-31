f4dbf736602752e293e8810a169170a95d8b40ae
unknown <usuario@GFI10297.gfi.es>
2020-01-31 13:28:52 +0100

    dhgsjgsjd

---
+++ Test Suites/Pruebaaa.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Pruebaaa</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>63fb5446-3414-4f07-be28-6d273066ba5d</testSuiteGuid>
+   <testCaseLink>
+      <guid>1cf23d07-28a7-447f-8fef-40ed3ec32204</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Prueba123</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
